3dd369378fd9c137ad304f305a4d99f1cc22af48
Endre Mella <mellaendre@gmail.com>
2025-10-07 13:31:16 +0200

    updated the code, and added comments

--- arduino_folgelinje/arduino_folgelinje.ino
+++ arduino_folgelinje/arduino_folgelinje.ino
@@ -1,35 +1,28 @@
-
-// Motor Driver Pins
 #define AIN1 11
-#define BIN1 10 
+#define BIN1 10
 #define AIN2 9
 #define BIN2 6
-#define PWMA 3 // PMW motor A
-#define PWMB 5 // PMW motor B
+#define PWMA 3
+#define PWMB 5
 #define STBY 2
 
-// Pin for emitter
 #define emitter 12
-
-// Zumo reflection array libary
 #include <QTRSensors.h>
 
 QTRSensors qtr;
 
-//Amount of senors
 const uint8_t SensorCount = 6;
-//Sensor pins is anlog pins A0-A5 turned to digital pins
 uint8_t qtrPins[SensorCount] = {14, 15, 16, 17, 18, 19};
 
 unsigned int sensorValues[SensorCount];
 
-int max_speed = 150;  
-int min_speed = 60; 
 
 float error = 0;
 float last_error = 0;
 
 int lastSide = 1;
+
+int sensorSum = 0;
 
 
 void setup() {
@@ -77,20 +70,22 @@ void loop() {
   // Print sensor readings
   for (uint8_t i = 0; i < 6; i++) {
     Serial.print(sensorValues[i]);
-    Serial.print('\t');
+    Serial.print("-");
   }
-  Serial.println();
+  Serial.println(); 
 
-  //Turning Sensor values between 0-2500 into binary values 1 if over 1750 and 0 if lower
+
+  // If sensor value is over 1400, we sett the value of the sensor to 1, if else, its not the line and we sett the value to 0
   int binValues[SensorCount];
   for (uint8_t i = 0; i < SensorCount; i++) {
-    if (sensorValues[i] > 1750) {
+    if (sensorValues[i] > 1400) {
       binValues[i] = 1;   // line
     } else {
       binValues[i] = 0;   // background
     }
   }
 
+  //Weighted sum is based on all binary values index, and if we deivde the weighted sum with sum we get the posistion.
   long weightedSum = 0;
   long sum = 0;
   for (uint8_t i = 0; i < SensorCount; i++) {
@@ -101,69 +96,60 @@ void loop() {
   unsigned long lastSearchTime = 0;
   const int searchInterval = 50;
   
-  //if sum is 0, sensor is out of route, Using last error to continue last correction 
+  //if sum is 0, the sensor cannot see any line, then it will turn based on the last error until it is back on the line
   if (sum == 0) {
       if (millis() - lastSearchTime > searchInterval) {
           lastSide = (last_error < 0) ? -1 : 1;
-          int searchSpeed = map(abs(last_error), 0, 3.5, 90, 50);
-          moveLeftMotor( searchSpeed * lastSide);
-          moveRightMotor( -searchSpeed * lastSide);
+          int searchSpeed = map(abs(last_error), 0, 3.5, 120, 60);
+          moveMotor(AIN1, AIN2, PWMA, searchSpeed * lastSide);
+          moveMotor(BIN1, BIN2, PWMB, -searchSpeed * lastSide);
           lastSearchTime = millis();
       }
       return;
+  } else {
+
+    float pos = (float)weightedSum / sum; // [1..6]
+    error = pos - 3.5;
+
+    lastSide = (error < 0) ? -1 : 1;
+    float delta_error = error - last_error; // derivative term
+    last_error = error;
+        
+    // Constants
+    const int MAX_SPEED = 140;
+    const int MIN_SPEED = 80;
+    const int PWM_MIN = 60;   // Consistent lower PWM limit everywhere
+
+    // PID gains
+    const float Kp = 30.0;
+    const float Kd = 20.0;
+    const float K = 6.0;
+
+    float correction = Kp * error + Kd * delta_error;
+    correction = constrain(correction, -120, 120);
+
+    // Dynamic speed scaling
+    float dynamic_factor = abs(error) + 0.8f * abs(delta_error);
+
+    // Map dynamic factor to speed (reversed scale!)
+    int base_speed = map(dynamic_factor, 0, K, MAX_SPEED-correction, PWM_MIN);
+    base_speed = constrain(base_speed, MIN_SPEED, MAX_SPEED);
+ 
+    // Apply correction
+    int leftSpeed  = constrain(base_speed - correction, PWM_MIN, MAX_SPEED);
+    int rightSpeed = constrain(base_speed + correction, PWM_MIN, MAX_SPEED);
+
+    moveMotor(AIN1, AIN2, PWMA, -leftSpeed);
+    moveMotor(BIN1, BIN2, PWMB, -rightSpeed);
   }
-
-  float pos = (float)weightedSum / sum;   // [1..6]
-  error = pos - 3.5;
-
-  lastSide = (error < 0) ? -1 : 1;
-  float delta_error = error - last_error;
-  last_error = error;
-
-  float Kp = 30.0;
-  float Kd = 10.0;      
-  float k  = 10.0;
-
-  float correction = Kp * error + Kd * delta_error;
-  correction = constrain(correction, -120, 120);
-
-  float dynamic_factor = abs(error) + 0.8 * abs(delta_error);
-
-  int base_speed = map(dynamic_factor, 0, k, max_speed, 60); 
-  base_speed = constrain(base_speed, min_speed, max_speed);
-
-
-  int leftSpeed  = base_speed - correction;
-  int rightSpeed = base_speed + correction;
-
-  // Limit speed range
-  leftSpeed  = constrain(leftSpeed, 50, 200);
-  rightSpeed = constrain(rightSpeed, 50, 200);
-
-
-  Serial.println(leftSpeed);
-  Serial.println(rightSpeed);
-
-  //moving both motors based on speed calculated
-  moveLeftMotor(leftSpeed);
-  moveRightMotor(rightSpeed);
-  
-
 }
-
-void stop(int stopTime) {
-  moveMotor(AIN1, AIN2, PWMA, 0);
-  moveMotor(BIN1, BIN2, PWMB, 0);
-  delay(stopTime);
-}
-
 
 void moveMotor(int in1, int in2, int pwm, int speed) {
-  if (speed < 0) {
+  if (speed > 0) {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     analogWrite(pwm, speed);
-  } else if (speed > 0) {
+  } else if (speed < 0) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     analogWrite(pwm, -speed);
@@ -173,12 +159,4 @@ void moveMotor(int in1, int in2, int pwm, int speed) {
     digitalWrite(in2, LOW);
     analogWrite(pwm, 0);
   }
-}
-
-void moveRightMotor(int speed) {
-  moveMotor(BIN1, BIN2, PWMB, speed);
-}
-
-void moveLeftMotor(int speed) {
-  moveMotor(AIN1, AIN2, PWMA, speed);
 }
